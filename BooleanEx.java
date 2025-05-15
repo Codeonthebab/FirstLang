7a29543854996f9b04465a8da696872e6b2a0a13
Codeonthebab <kjanny152@gmail.com>
2025-05-15 18:55:16 +0900

    Boolean.java
    My first VariableJAVA

---
+++ BooleanEx.java
@@ -0,0 +1,19 @@
+package com.variable;
+
+/*	boolean : 자바에서 유일한 논리 자료형
+ * 	(*크기는 1byte이지만 실제 사용은 1bit 사용)
+ *	참이냐 거짓이냐로 주로 사용한다
+ */
+
+
+public class BooleanEx {
+
+	public static void main(String[] args) {		
+		boolean a = false;
+		boolean b = true;
+		
+		System.out.println("" +b);
+		System.out.println("" +a);
+	}
+
+}
