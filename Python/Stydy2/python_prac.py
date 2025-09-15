3dbd3b77fe1a3645307411360bbbd56b6fb7b18b
Codeonthebab <kjanny152@gmail.com>
2025-09-15 12:41:52 +0900

    [Study] jyuku_python1
    여기도 형,변환이 있답니다

---
+++ Python/Stydy2/python_prac.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+
+result = 5 + 3
+print("5 + 3 =", result)
+
+a = 10
+b = 20
+
+result = a * b
+print(f"{a} * {b} =", result)
+result = (float)(b / a)
+print(f"{b} / {a} =", result)
+result = b - a
+print(f"{b} - {a} =", result)
+result = b // a
+print(f"{b} // {a} =", result)
+result = b % a
+print(f"{b} % {a} =", result)
+
