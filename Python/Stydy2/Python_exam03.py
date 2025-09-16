76177153b64093aad5e2b1d1f806ec7e47adae35
Codeonthebab <kjanny152@gmail.com>
2025-09-16 12:50:34 +0900

    [Study] Python Exam elif

---
+++ Python/Stydy2/Python_exam03.py
@@ -0,0 +1,20 @@
+# 숫자를 입력받아 무지개 일곱 빛 중 하나를 출력하는 프로그램을 작성하시오.
+num = int(input("1~7 사이의 숫자를 입력하세요: "))
+
+if num == 1:
+    print("빨강")
+elif num == 2:
+    print("주황")
+elif num == 3:
+    print("노랑")
+elif num == 4:
+    print("초록")
+elif num == 5:
+    print("파랑")
+elif num == 6:
+    print("남색")
+elif num == 7:
+    print("보라")
+else:
+    print("1~7 사이의 숫자를 입력하세요.")
+# 무지개 색 출력 프로그램
