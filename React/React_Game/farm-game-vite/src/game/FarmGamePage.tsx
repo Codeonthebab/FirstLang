1dd335fd074dc23bc64d365cd47ba05bb80254cb
Codeonthebab <kjanny152@gmail.com>
2025-10-07 18:56:58 +0900

    [feat] 타일셋 그래픽 적용

--- React/React_Game/farm-game-vite/src/game/FarmGamePage.tsx
+++ React/React_Game/farm-game-vite/src/game/FarmGamePage.tsx
@@ -9,28 +9,28 @@ class GameScene extends Phaser.Scene {
   preload() {
     // Tiled에서 사용할 '마스터 팔레트' 이미지
     this.load.image(
-      'master_tiles',
+      'terrain_tiles',
       'assets/Sunnyside_World_ASSET_PACK_V2.1/Sunnyside_World_Assets/Tileset/spr_tileset_sunnysideworld_16px.png'
     );
 
     // Tiled JSON 맵 파일 경로
     this.load.tilemapTiledJSON (
         'farm_map',
-        'assets/sunnyside_master_tileset.json'
+        'assets/Sunnyside_World_ASSET_PACK_V2.1/farm.json'
     );
   }
   create() {
-    const map = this.make.tilemap({ key: "farm_map" });
+    const map = this.make.tilemap({ key: 'farm_map' });
 
     // Tiled에서 타일셋에 부여한 이름('Terrain')과 preload의 이미지 키('terrain_tiles')를 연결
     const tileset = map.addTilesetImage(
-      "sunnyside_master_tileset", 
-      "master_tiles"
+      'terrain', 
+      'terrain_tiles'
     );
 
     // Tiled에서 만든 레이어 이름('Ground')을 사용
     if (tileset) {
-      map.createLayer("Ground", tileset, 0, 0);
+      map.createLayer('Ground', tileset, 0, 0);
     }
   }
 }
